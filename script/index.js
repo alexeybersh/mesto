70e04f23b40f20c95cf1ae1266a4f680f7371681
Alexey Bersh <balexworks@gmail.com>
2023-06-15 15:24:39 +0300

    hotfix

--- script/index.js
+++ script/index.js
@@ -5,11 +5,11 @@ const editButtonElement = document.querySelector(".profile__edit-button");
 const closeButtonElement = document.querySelector(".popup__close-button");
 const saveButtonElement = document.querySelector(".popup__save-button");
 const popupElement = document.querySelector(".popup");
+const inputName = document.querySelector(".popup__profile-title_input_text");
+const inputjob = document.querySelector(".popup__profile-subtitle_input_text");
 
 editButtonElement.addEventListener("click", () => {
   popupElement.classList.toggle("popup_opened");
-  const inputName = document.querySelector(".popup__profile-title");
-  const inputjob = document.querySelector(".popup__profile-subtitle");
 
   inputName.value = nameInput.textContent;
   inputjob.value = jobInput.textContent;
@@ -17,8 +17,6 @@ editButtonElement.addEventListener("click", () => {
 
 function handleFormSubmit(evt) {
   evt.preventDefault();
-  const inputName = document.querySelector(".popup__profile-title");
-  const inputjob = document.querySelector(".popup__profile-subtitle");
 
   inputName.value = inputName.value;
   inputjob.value = inputjob.value;
