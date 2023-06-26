ed1cc9d157592e0156ac7cfc19f7d4ba89d03b4d
Alexey Bersh <balexworks@gmail.com>
2023-06-26 21:58:46 +0300

    start

--- script/index.js
+++ script/index.js
@@ -2,10 +2,14 @@ let nameInput = document.querySelector(".profile__title");
 let jobInput = document.querySelector(".profile__subtitle");
 
 const editButtonElement = document.querySelector(".profile__edit-button");
-const closeButtonElement = document.querySelector(".popup__close-button");
-const saveButtonElement = document.querySelector(".popup__save-button");
-const popupElement = document.querySelector(".popup");
-const formElement = document.querySelector(".popup__form");
+const closeButtonElement = document.querySelector(
+  ".popup__close-button_edit_profile"
+);
+const saveButtonElement = document.querySelector(
+  ".popup__save-button_edit_profile"
+);
+const popupElement = document.querySelector(".popup_edit_profile");
+const formElement = document.querySelector(".popup__form_edit_profile");
 const inputName = document.querySelector(".popup__profile_input-name_text");
 const inputjob = document.querySelector(".popup__profile_input-job_text");
 
@@ -34,3 +38,29 @@ closeButtonElement.addEventListener("click", () => {
 function popupOpened() {
   popupElement.classList.toggle("popup_opened");
 }
+
+const addButtonImage = document.querySelector(".profile__add-button");
+const closeButtonElementImage = document.querySelector(
+  ".popup__close-button_add_image"
+);
+const saveButtonElementImage = document.querySelector(
+  ".popup__save-button_add_image"
+);
+const popupElementImage = document.querySelector(".popup_add_image");
+const formElementImage = document.querySelector(".popup__form_add_image");
+const inputNameImage = document.querySelector(
+  ".popup__profile_input-name-image_text"
+);
+const inputLinkImage = document.querySelector(".popup__profile_link_text");
+
+function popupOpenedAdd() {
+  popupElementImage.classList.toggle("popup_opened");
+}
+
+addButtonImage.addEventListener("click", () => {
+  popupOpenedAdd();
+});
+
+closeButtonElementImage.addEventListener("click", () => {
+  popupOpenedAdd();
+});
