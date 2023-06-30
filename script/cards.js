41cff6cb947c61784c6f644f2ad109764c0aaa6b
Alexey Bersh <balexworks@gmail.com>
2023-06-30 18:23:57 +0300

    hotfix

--- script/cards.js
+++ script/cards.js
@@ -8,19 +8,19 @@ const initialeElements = [
     link: "https://beautifoto.ru/wp-content/uploads/2019/06/9-11.jpg",
   },
   {
-    name: "Иваново",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+    name: "Качели на пляжу",
+    link: "https://sportishka.com/uploads/posts/2022-11/1667430606_1-sportishka-com-p-plyazh-s-palmami-oboi-1.jpg",
   },
   {
-    name: "Камчатка",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+    name: "Пляж и море",
+    link: "http://images.ua.prom.st/2575163499_w640_h640_fotooboi-3d-more.jpg",
   },
   {
-    name: "Холмогорский район",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+    name: "Мальдивы",
+    link: "https://w.forfun.com/fetch/39/39be93396e708deb97bdb8b0d538a13e.jpeg",
   },
   {
-    name: "Байкал",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+    name: "Сейшелы",
+    link: "https://wp-s.ru/wallpapers/2/91/379101226417646/fotografiya-palmy-plyazha-okeana-i-solnca.jpg",
   },
 ];
