4bf8e6b1f58f4fe3cf9f444201b5c3e3bc62377f
Bershatskiy Alexey <Alexey.Bershatskiy@barilla.com>
2023-07-30 19:18:52 +0300

    start sprint 7

---
+++ script/content.js
@@ -0,0 +1,26 @@
+const initialeElements = [
+    {
+      name: "Пальмы на пляже",
+      link: "https://w.forfun.com/fetch/4c/4c730760279faf92fd9e0bb260a0a2d9.jpeg",
+    },
+    {
+      name: "Закат на море",
+      link: "https://beautifoto.ru/wp-content/uploads/2019/06/9-11.jpg",
+    },
+    {
+      name: "Качели на пляжу",
+      link: "https://s1.1zoom.ru/b6343/302/Sea_Tropics_Palms_Beach_Swing_531524_3840x2160.jpg",
+    },
+    {
+      name: "Пляж и море",
+      link: "http://images.ua.prom.st/2575163499_w640_h640_fotooboi-3d-more.jpg",
+    },
+    {
+      name: "Мальдивы",
+      link: "https://w.forfun.com/fetch/39/39be93396e708deb97bdb8b0d538a13e.jpeg",
+    },
+    {
+      name: "Сейшелы",
+      link: "https://wp-s.ru/wallpapers/2/91/379101226417646/fotografiya-palmy-plyazha-okeana-i-solnca.jpg",
+    },
+  ];
